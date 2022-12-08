6a05fa2ab5cd0dc8e6ca517b6abf54a2ce9787a1
GabrielMeloBatista <gabrielmelobatisa@hotmail.com>
2022-12-08 11:31:03 -0300

    Concertado o zoom

--- Assets/Scripts/UIBoneViewer.cs
+++ Assets/Scripts/UIBoneViewer.cs
@@ -6,24 +6,32 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class UIBoneViewer: MonoBehaviour
+public class UIBoneViewer : MonoBehaviour
 {
     Vector3 firtPoint;
     Vector3 secondPoint;
-    float xAngle, yAngle, xAngleTemp, yAngleTemp, zoomOutMin, zoomOutMax;
+    public float xAngle, yAngle, xAngleTemp, yAngleTemp, zoomOutMin, zoomOutMax;
     [SerializeField] Camera boneCamera;
+    public float buffer = 1000;
+
+    public void closeViewer()
+    {
+        Destroy(this);
+    }
+
     void Start()
     {
-        xAngle= 0;
-        yAngle= 0;
-        transform.rotation= Quaternion.Euler(yAngle, xAngle, 0);
+        xAngle = 0;
+        yAngle = 0;
+        transform.rotation = Quaternion.Euler(yAngle, xAngle, 0);
         zoomOutMin = 0.06f;
-        zoomOutMax = 1;
+        zoomOutMax = 0.2f;
+        boneCamera = GameController.getInstance().getCamera();
     }
 
     void Update()
     {
-        if(Input.touchCount == 2)
+        if (Input.touchCount == 2)
         {
             Touch touchZero = Input.GetTouch(0);
             Touch touchOne = Input.GetTouch(1);
@@ -36,32 +44,33 @@ public class UIBoneViewer: MonoBehaviour
 
             float diference = currentMagnitude - prevMagnitute;
 
-            zoom(diference + 0.01f);
+            zoom(diference / buffer);
         }
         else if (Input.touchCount > 0)
         {
             // Quando se a toque, ve a posição inicial dele
-            if(Input.GetTouch(0).phase== TouchPhase.Began)
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
             {
                 firtPoint = Input.GetTouch(0).position;
                 xAngleTemp = xAngle;
                 yAngleTemp = yAngle;
             }
             // caso mova, verifica aonde ele esta indo
-            if(Input.GetTouch(0).phase== TouchPhase.Moved) {
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
                 secondPoint = Input.GetTouch(0).position;
                 // Ajusta os angulos baseados no movimento do toque
                 xAngle = xAngleTemp + (secondPoint.x - firtPoint.x) * 180.0f / Screen.width;
                 yAngle = yAngleTemp + (secondPoint.y - firtPoint.y) * 90.0f / Screen.height;
-                
+
                 // Baseado nisto, ele rotaciona o GameObject que tem este script
                 transform.rotation = Quaternion.Euler(yAngle, xAngle, 0.0f);
             }
         }
-        
-        void zoom(float increment)
-        {
-            boneCamera.orthographicSize = Mathf.Clamp(boneCamera.orthographicSize - increment, zoomOutMin, zoomOutMax);
-        }
+    }
+
+    void zoom(float increment)
+    {
+        boneCamera.orthographicSize = Mathf.Clamp(boneCamera.orthographicSize - increment, zoomOutMin, zoomOutMax);
     }
 }
