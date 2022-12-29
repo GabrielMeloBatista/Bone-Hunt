565401e041f5f9799bc2f18e119bca1b4dcfdd28
GabrielMeloBatista <gabrielmelobatisa@hotmail.com>
2022-12-29 08:52:56 -0300

    Modificação nos Import

--- Assets/UI Toolkit/Merchant.cs
+++ Assets/UI Toolkit/Merchant.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
-using static UnityEditor.Progress;
 
 public class Merchant : MonoBehaviour
 {
